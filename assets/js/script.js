559793bd45cd3778fd23ce820276a575714c563f
lisarozo <erozo190@gmail.com>
2021-08-28 12:06:10 -0500

    added a generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,39 @@ function generatePassword() {
   var lowercase = confirm("Do you want to include lower case characters?");
   var uppercase = confirm("Do you want to include upper case letters?");
   var numeric = confirm("Do you want to include numeric characters?");
+  console.log(lowercase, uppercase, numeric)
+  if (lowercase, uppercase, numeric) {
+    var myArray=[]
+  for (let index = 0; index < length; index++) {
+    var element = Math.floor(Math.random() * 10);
+    console.log (index % 2 == 0) 
+   if ((index % 2 == 0)==true) {
+     myArray.push("a")
+   }else{
 
+     myArray.push(element)
+   }
+    
+  }
+    
+  }
   // if(typeof x === 'string' && typeof y === 'string' && typeof z === 'string') {
   // console.log("My if was true")
   // }
   //   // else {
-    var myArray=[]
-  for (let index = 0; index < length; index++) {
-    var element = Math.floor(Math.random() * 10);
-    myArray.push(element)
+  //   var myArray=[]
+  // for (let index = 0; index < length; index++) {
+  //   var element = Math.floor(Math.random() * 10);
+  //   myArray.push(element)
     
-  }
+  // }
 
   var computerTurn =  Math.floor(Math.random() * length);
 
-  console.log(myArray);
-
+  // console.log(myArray);
+  var finalPassword = myArray.toString(); 
+  console.log(finalPassword)
+  return finalPassword
 
   // var userTurn = prompt('Enter ').toLowerCase();
 
@@ -41,10 +58,8 @@ function generatePassword() {
  
 
 
-  for(var i = 0; i < 10; i++) {
-    var num =Math.floor(Math.random() * 10);
-    console.log (num);
-}
+ 
+    
 function CheckPassword(inputtxt) {
   var passw = "Li$aco2021";
   if (inputtxt.value.match(passw)) {
